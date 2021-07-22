d29b300091aaa88f9741d577ff7540a32262526e
MAC <mac@DREAMSs-iMac-Pro.local>
2021-07-22 17:26:28 +0530

    added code

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { Product } from "./products";
 export { Header, Footer, DashboardFilter } from "./common";
 export { ItemList, Item } from "./items";
+export { LoginForm , RegistrationForm , Loginscreen} from "./Login";
